da2561940667670ddd8d500b888553f4875aa93e
Natanael <natanaeldepaulo123@gmail.com>
2022-11-21 12:59:07 -0300

    Created connection dataBase

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import { router } from './router';
+import { routes } from './app/routers';
+import { ConnectDb } from './app/database';
 
 export const app = express();
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-app.use(router);
+app.use(routes);
+app.use((req, res, next) => ConnectDb
+	.then(() => next())
+	.catch(err => next(err)));
