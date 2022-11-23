049b80c457184608f49cb2a6b7cdb1cbf53b3256
Natanael <natanaeldepaulo123@gmail.com>
2022-11-23 01:38:54 -0300

    doc: comments packages

--- api/src/app.ts
+++ api/src/app.ts
@@ -6,10 +6,15 @@ import { ConnectDb } from './app/database';
 
 export const app = express();
 
+// CORS allow resource sharing between different origins on each request
 app.use(cors());
+
+// adding Helmet to improve HTTP headers security
 app.use(helmet());
 app.use(express.json());
 app.use(routes);
+
+// connect db
 app.use((req, res, next) => ConnectDb
 	.then(() => next())
 	.catch(err => next(err)));
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,9 @@
-
 import { app } from './app';
 
 const port = 3001;
 
 app.listen(process.env.PORT || port, () => console.log(`🚀 Server is running on http://localhost:${process.env.PORT || port}`));
 
-	
 console.log('ENV VARIABLES:');
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 console.log(`PORT: ${process.env.PORT}`);
