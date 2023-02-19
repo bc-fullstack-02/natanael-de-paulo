93610b8cdf4cd4b404b2364360a6690983046239
Natanael <natanaeldepaulo123@gmail.com>
2023-02-19 19:35:30 -0300

    refact: modified controller to create user

--- api/src/app/controllers/post/CreatePostController.ts
+++ api/src/app/controllers/post/CreatePostController.ts
@@ -3,19 +3,19 @@ import { Profile } from '../../models/Profile';
 import { CreatePostService } from '../../services/post/CreatePostService';
 
 class CreatePostController {
-	async handle(req: Request, res: Response){
+	async handle(req: Request, res: Response) {
 		const imagePath = req.file?.filename;
-		const user_id  = req.user_id;
+		const user_id = req.user_id;
 		const { title, description } = req.body;
 		const profile = await Profile.findOne({ user: user_id });
-		const createPostService  = new CreatePostService();
+		const createPostService = new CreatePostService();
 
-		if(!title) return res.status(400).json({ error: 'Title is required!'});
-		if(!description) return res.status(400).json({ error: 'Description is required!'});
-		
-		const newPost = await createPostService.execute({title, description, user_id, imagePath});
+		if (!title) return res.status(400).json({ error: 'Title is required!' });
+		if (!description) return res.status(400).json({ error: 'Description is required!' });
+
+		const newPost = await createPostService.execute({ title, description, user_id, imagePath });
 		await req.publish('post', profile?.followers, newPost);
-		
+
 		res.status(201).json(newPost);
 	}
 }
