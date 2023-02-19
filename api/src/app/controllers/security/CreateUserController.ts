5f959319074be76219094dc6d90682682cb802c6
Natanael <natanaeldepaulo123@gmail.com>
2023-02-19 20:34:47 -0300

    refact: variable created to instantiate the user creation service

--- api/src/app/controllers/security/CreateUserController.ts
+++ api/src/app/controllers/security/CreateUserController.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from 'express';
 import { validateUserBody } from '../../shared/utils/validators/ValidateUserBody';
-import { CreateUserService } from '../../services/security/CreateUserService';
+import { createUserService } from '../../services/security/CreateUserService';
 
 class CreateUserController {
 	async handle(req: Request, res: Response) {
 		await validateUserBody.create(req.body);  
-		const createUserService = new CreateUserService();
 		const newUser = await createUserService.execute(req.body);
 		return res.json(newUser);
 	}
