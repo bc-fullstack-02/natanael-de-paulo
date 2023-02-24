07280583882f7591495c35f6654df2794fdc0a80
Natanael <natanaeldepaulo123@gmail.com>
2023-02-23 23:32:06 -0300

    feat: create more validates

--- api/src/app/shared/utils/validators/index.ts
+++ api/src/app/shared/utils/validators/index.ts
@@ -12,6 +12,18 @@ class Validate{
 		if (userAlreadyExists) throw new BadRequestException('User already exists!');
 		return true;
 	}
+
+	emailFormat(email: string) {
+		const regEmail = /^[a-zA-Z0-9_.+]+(?<!^[0-9]*)@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+		if (!regEmail.test(String(email))) throw new BadRequestException('Email format is invalid!');
+		return true;
+	}
+
+	async emailIsRegistered(email: string) {
+		const validatedEmail = await User.find({}).where('email').equals(email);
+		if (validatedEmail) throw new BadRequestException(`e-mail ${email} is already registered!`);
+		return true;
+	}
 }
 
 export const validate = new Validate();
