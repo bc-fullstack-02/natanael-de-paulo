d2d83fbfa50aac0f83217228d27df24337da6888
Natanael <natanaeldepaulo123@gmail.com>
2023-03-04 07:34:09 -0300

    refact: applying SOLID practices and clean code

--- api/src/app/controllers/security/CreateUserController.ts
+++ api/src/app/controllers/security/CreateUserController.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from 'express';
 import { validateUserBody } from '../../shared/utils/validators/ValidateUserBody';
 import { createUserService } from '../../services/security/CreateUserService';
+import { createProfileService } from '../../services/profile/CreateProfileService';
+import { updateUserService } from '../../services/user/UpdateUserService';
 
 class CreateUserController {
 	async handle(req: Request, res: Response) {
 		await validateUserBody.create(req.body);  
-		const newUser = await createUserService.execute(req.body);
-		return res.json(newUser);
+		const user = await createUserService.execute(req.body);
+		const profile = await createProfileService.execute(user, req.body.profile);
+		const userUpdated = await updateUserService.insertProfileInUser({ user, profile });
+		return res.status(201).json(userUpdated);
 	}
 }
 
--- api/src/app/services/security/CreateUserService.ts
+++ api/src/app/services/security/CreateUserService.ts
@@ -1,14 +1,11 @@
-import { CreateUserType } from '../../shared/utils/types/UserTypes';
+import { UserType } from '../../shared/types/UserTypes';
 import { userRepository } from '../../repository/UserRepository';
 import bcrypt from 'bcrypt';
-import { profileRepository } from '../../repository/ProfileRepository';
 
 class CreateUserService {
-	async execute({user, password, profile}: CreateUserType) {
+	async execute({user, password}: UserType) {
 		const passwordHash = await bcrypt.hash(password, 10);
 		const newUser = await userRepository.create({user, passwordHash});
-		await profileRepository.create(newUser, profile);
-		
 		return newUser;
 	}
 }
