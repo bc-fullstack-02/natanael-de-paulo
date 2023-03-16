5844e3a15dcefde21128e17808a9ef4f28b2c9b3
Natanael de Paulo <natanaeldepaulo123@gmail.com>
2023-03-15 22:18:02 -0300

    teste

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -13,7 +13,7 @@ export function Button({ children, asChild, className, ...props} : ButtonProps){
 
 	return (
 		<Comp className={clsx(
-			'py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus-within:right-2 ring-white',
+			'py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus-within:right-2 ring-white ',
 			className
 		)} {...props} >
 			{ children }
