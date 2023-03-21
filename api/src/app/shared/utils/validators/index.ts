3022bd450533d82452d5450567bd7527c41168c4
Natanael de Paulo <natanaeldepaulo123@gmail.com>
2023-03-21 09:18:45 -0300

    fix: resolved bug email validation

--- api/src/app/shared/utils/validators/index.ts
+++ api/src/app/shared/utils/validators/index.ts
@@ -32,7 +32,7 @@ class Validate{
 	}
 
 	async emailIsRegistered(email: string) {
-		const validatedEmail = await User.find({}).where('email').equals(email);
+		const validatedEmail = await User.findOne({}).where('email').equals(email);
 		if (validatedEmail) throw new BadRequestException('Email is already registered!');
 		return true;
 	}
