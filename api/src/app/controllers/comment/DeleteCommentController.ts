674e7d889d7ef5322f08342cf718ce5b8719d2fe
Natanael de Paulo <natanaeldepaulo123@gmail.com>
2023-03-27 14:17:14 -0300

    refact: delete validade params to controller

--- api/src/app/controllers/comment/DeleteCommentController.ts
+++ api/src/app/controllers/comment/DeleteCommentController.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express';
 import { deleteCommentService } from '../../services/comment/DeleteCommentService';
 import { updateCommentsToPostService } from '../../services/comment/UpdateCommentsToPostService';
-import { validateCommentBody } from '../../shared/utils/validators/ValidateCommentBody';
+import { validateParams } from '../../shared/utils/validators/ValidateParams';
 
 class DeleteCommentController {
 	async handle(req: Request, res: Response){
 		const { post_id, comment_id } = req.params;
-		await validateCommentBody.fields(post_id, comment_id);
+		await Promise.all([
+			validateParams.postId(post_id),
+			validateParams.commentId(comment_id)
+		]);
 		
 		await Promise.all([
 			await deleteCommentService.execute({post_id, comment_id}),
