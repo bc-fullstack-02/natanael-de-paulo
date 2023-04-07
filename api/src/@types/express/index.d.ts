4d3a9615db0919f0d4075343fd61ca335d6ea00f
Natanael de Paulo <natanaeldepaulo123@gmail.com>
2023-04-07 09:34:59 -0300

    minio implemented

--- api/src/@types/express/index.d.ts
+++ api/src/@types/express/index.d.ts
@@ -1,6 +1,7 @@
 declare namespace Express{
   export interface Request{
     user_id: string;
+    profile_id: string;
     publish: Promise;
     file: Promise;
   }
--- api/src/app/services/post/CreatePostService.ts
+++ api/src/app/services/post/CreatePostService.ts
@@ -2,8 +2,8 @@ import { postRepository } from '../../repository/PostRepository';
 import { CreatePostType } from '../../shared/types/PostTypes';
 
 class CreatePostService {
-	async execute({ title, description, profile, imagePath }: CreatePostType) {
-		const post = await postRepository.create({title, description, profile, imagePath});
+	async execute({ title, description, profile, imageUrl, image }: CreatePostType) {
+		const post = await postRepository.create({title, description, profile, imageUrl, image});
 		return post;
 	}
 	//rota de postlike => req.publish('post-like', [args.profile], args)
